72a85a16abc1e51b4979bb0a206a9944be22b54e
Adithya Ramachandran <adithyapramachandran@gmail.com>
2026-02-23 19:47:41 +0530

    Update rag_pipeline.py

--- backend/rag_pipeline.py
+++ backend/rag_pipeline.py
@@ -1,1 +1,30 @@
-Implement RAG retrieval and answer generation pipeline
+# Implement RAG retrieval and answer generation pipeline
+import os
+from app.db.vector_store import get_db
+from openai import OpenAI
+
+client = OpenAI()
+
+def answer_question(question):
+    db = get_db()
+
+    docs = db.similarity_search(question, k=3)
+
+    context = "\n".join([d.page_content for d in docs])
+
+    prompt = f"""
+Answer using context.
+
+Context:
+{context}
+
+Question:
+{question}
+"""
+
+    res = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role":"user","content":prompt}]
+    )
+
+    return res.choices[0].message.content
